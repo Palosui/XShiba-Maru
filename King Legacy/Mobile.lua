ae275eeadac0a8edc966cf9b764192b021dc8366
Palosui <144542739+Palosui@users.noreply.github.com>
2023-10-30 23:11:37 +0700

    Create Mobile.lua

---
+++ King Legacy/Mobile.lua
@@ -0,0 +1,6 @@
+
+if game.PlaceId==4520749081 or game.PlaceId==6381829480 or game.PlaceId==5931540094 or game.PlaceId==6596144663  
+  then
+  --King Legacy
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Palosui/Auto-Farm/main/Free'))()
+end
